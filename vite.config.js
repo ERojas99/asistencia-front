54d72926fe91d848e014b085dd1d962669dd52ae
Tecon 1 <tecon1@Tecons-MacBook-Air.local>
2025-05-07 23:50:10 -0500

    Agregar host permitido para Render

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    allowedHosts: ['asistencia-front.onrender.com']
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json']
